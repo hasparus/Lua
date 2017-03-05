34fbb2f033471c45ad63510644820acd5fd15131
hasparus <hasparus@gmail.com>
2017-03-05 23:41:06 +0100

    Init and finish list.

---
+++ Lista 01/main.lua
@@ -0,0 +1,45 @@
+--local table = require("table")
+
+function map(fun, tab) 
+    local res = {}
+    for i, x in ipairs(tab) do
+        res[i] = fun(x)
+    end 
+    return res 
+end 
+
+function printf(x)
+  local old_tostring = tostring
+  function tostring(x) 
+    if type(x) == 'table' then
+      return ('{' .. table.concat(
+              map(tostring, x), ", ", 1, #x) 
+              .. '}')
+    end
+    return old_tostring(x)
+  end
+  print(x)
+end
+
+printf({{'WOW'}, 'to', 'są', 'zagnieżdżone', {{'tablice', {true}}}});
+print({{'Super'}, 'na', 'zwykłym', 'princie', {{'dziala', {'bo przepisałem tostring tabla, żeby traktować je jak wartość. To zabija możliwość wypisywania ich jako słownika'}}}});
+
+do
+  local fibs = {0, 1}
+  function fibonacci(n)
+    n = n + 1
+    while (n > #fibs) do
+      fibs[#fibs + 1] = fibs[#fibs] + fibs[#fibs - 1]
+      print('Adding fibs[' .. #fibs .. '] = ' .. fibs[#fibs] .. '.')
+    end
+    return fibs[n]
+  end
+end
+print(fibonacci(0))
+print(fibonacci(1))
+print(fibonacci(2))
+print(fibonacci(5))
+print(fibonacci(9))
+print(fibonacci(1))
+print(fibonacci(5))
+print(fibs);
