f4da6154de71c509769484372c9005f08149a988
hasparus <hasparus@gmail.com>
2017-04-05 10:17:50 +0200

    Minor additions to list 5.

--- Lista 05/main.lua
+++ Lista 05/main.lua
@@ -2,21 +2,45 @@ require('past04')
 
 local function chain(...)
   return function(a, i)
-      i[2] = i[2] + 1
-      while i[2] > #a[ i[1] ] do
-        i[1] = i[1] + 1
-        i[2] = 1
+    i[2] = i[2] + 1
+    while i[2] > #a[ i[1] ] do
+      i[1] = i[1] + 1
+      i[2] = 1
 
-        if a[ i[1] ] == nil then
-          return
-        end
+      if a[ i[1] ] == nil then
+        return
       end
-      return i, a[ i[1] ][ i[2] ]
-    end, table.pack(...), {1, 0}
+    end
+    return i, a[ i[1] ][ i[2] ]
+  end, table.pack(...), {1, 0}
+end
+
+local function chainNoKey(...)
+  return function(state)
+    local a = state[1]
+    local i = state[2]
+
+    i[2] = i[2] + 1
+    while i[2] > #a[ i[1] ] do
+      i[1] = i[1] + 1
+      i[2] = 1
+
+      if a[ i[1] ] == nil then
+        return
+      end
+    end
+    return a[ i[1] ][ i[2] ]
+  end, {table.pack(...), {1, 0}}
 end
 
 ---[[
 for _, x in chain ({ 'a', 'b', 'c'} , {40 , 50} , {} , {6 , 7}) do
+  print(x)
+end
+
+print('---------------------')
+
+for x in chainNoKey ({ 'a', 'b', 'c'} , {40 , 50} , {} , {6 , 7}) do
   print(x)
 end
 --]]
@@ -40,6 +64,26 @@ local zip = function(...)
   end, table.pack(...), 1
 end
 
+local zipNoKey = function(...)
+  return function(state)
+    local arrs = state[1]
+    local i = state[2]
+    
+    local res = {}
+    for j = 1, arrs.n do
+      local v = arrs[j][i]
+      if v == nil then return end
+      res[j] = v
+    end
+    state[2] = i + 1
+    return res
+  end, {table.pack(...), 1}
+end
+
 for k, v in zip({1, 2}, {3, 4}) do
   print(k, v)
+end
+
+for v in zipNoKey({1, 2}, {3, 4}) do
+  print(v)
 end
